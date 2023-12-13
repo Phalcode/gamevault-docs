6715e555e130439cb5c10d4da730912660d91e2a
Alper Alkan <alper.alkan2000@gmail.com>
2023-12-13 23:32:34 +0100

    sh

--- src/pages/index.js
+++ src/pages/index.js
@@ -70,7 +70,7 @@ export default function Home() {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="the self-hosted gaming platform for alternatively obtained games"
+      description="the self-hosted gaming platform for drm-free games"
     >
       <HomepageHeader />
       <main>
